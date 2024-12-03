2377fbe3d2a51eb5b172718614c41254ba0e34e3
Alisson Santos <cnctalissonsantos@gmail.com>
2024-12-02 21:24:17 -0300

    fixed some bugs in montage

--- src/tstorgb/montage.py
+++ src/tstorgb/montage.py
@@ -70,18 +70,12 @@ if __name__ == "__main__":
     if out_path.exists() is False:
         out_path.mkdir()
 
-    # Get total of montages.
-    n = 0
-    total = np.sum([len(list(Path(k).glob("*/*/"))) for k in args.input_dir.split(",")])
-
     # Start making the montages.
     for in_path_str in args.input_dir.split(","):
         entity = Path(in_path_str)
 
         if entity.exists() is True:
             for variation in entity.glob("*/"):
-                n += 1
-                print(f"Montage: {n}/{total}")
                 with Image() as big_montage_img:
                     states = sorted([k.name for k in variation.glob("*/")])
                     if len(states) == 0:
@@ -95,14 +89,17 @@ if __name__ == "__main__":
                             )
                             continue
 
-                        nrow = int(np.sqrt(max_frame_number))
-                        ncol = np.ceil(max_frame_number / nrow)
                         with Image() as montage_img:
-                            for i in range(
-                                len(list(variation.glob(f"*.{args.input_extension}")))
-                            ):
+                            image_order = sorted(
+                                [
+                                    int(k.stem.rsplit("_image_", maxsplit=1)[-1])
+                                    for k in variation.glob(f"*.{args.input_extension}")
+                                ]
+                            )
+                            for index in image_order:
                                 imagepath = Path(
-                                    variation, f"{i}.{args.input_extension}"
+                                    variation,
+                                    f"{entity.name}_{variation.name}_image_{index}.{args.input_extension}",
                                 )
                                 with Image(filename=imagepath) as frame:
                                     frame.border(args.border_color, width=5, height=5)
@@ -111,35 +108,26 @@ if __name__ == "__main__":
                             montage_img.background_color = "transparent"
                             montage_img.compression_quality = args.image_quality
                             montage_img.montage(
-                                tile=f"{ncol}x",
                                 thumbnail="+0+0",
                             )
                             montage_img.border(
                                 color=args.border_color, width=5, height=5
                             )
-                            # Write label.
-                            montage_img.background_color = args.label_background_color
-                            montage_img.splice(
-                                x=0,
-                                y=0,
-                                width=0,
-                                height=256,
-                            )
-                            font_size = montage_img.width // len(variation.name)
-                            with Drawing() as ctx:
-                                ctx.font_family = args.font
-                                ctx.font_style = "italic"
-                                ctx.font_size = min(11 / 8 * font_size, 200)
-                                ctx.text_kerning = 8
-                                ctx.fill_color = args.font_color
-                                montage_img.annotate(
-                                    variation.name,
-                                    ctx,
-                                    left=64,
-                                    baseline=190,
-                                )
 
                             big_montage_img.image_add(montage_img)
+
+                        target = Path(out_path, entity.name)
+                        target.mkdir(parents=True, exist_ok=True)
+
+                        big_montage_img.background_color = "transparent"
+                        big_montage_img.compression_quality = args.image_quality
+                        big_montage_img.montage(tile="1x", thumbnail="+0+0")
+                        big_montage_img.save(
+                            filename=Path(
+                                target,
+                                f"{entity.name}_{variation.name}.{args.output_extension}",
+                            )
+                        )
                     else:
                         max_frame_number = max(
                             [
@@ -211,15 +199,15 @@ if __name__ == "__main__":
 
                                 big_montage_img.image_add(montage_img)
 
-                    target = Path(out_path, variation.relative_to(entity.parent))
-                    target.mkdir(parents=True, exist_ok=True)
+                        target = Path(out_path, variation.relative_to(entity.parent))
+                        target.mkdir(parents=True, exist_ok=True)
 
-                    big_montage_img.background_color = "transparent"
-                    big_montage_img.compression_quality = args.image_quality
-                    big_montage_img.montage(tile="1x", thumbnail="+0+0")
-                    big_montage_img.save(
-                        filename=Path(target, f"sheet.{args.output_extension}")
-                    )
+                        big_montage_img.background_color = "transparent"
+                        big_montage_img.compression_quality = args.image_quality
+                        big_montage_img.montage(tile="1x", thumbnail="+0+0")
+                        big_montage_img.save(
+                            filename=Path(target, f"sheet.{args.output_extension}")
+                        )
 
         else:
             print(
